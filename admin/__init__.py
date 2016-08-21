ebb4510d2bbae5a105224ee6f0597f112279bbaa
marzim <marvin.casagnap@gmail.com>
2016-08-21 11:33:35 -0400

    commit changes

---
+++ admin/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+def add_routes(app=None):
+    blueprint = Blueprint('admin', __name__, static_url_path='/admin/static', static_folder='./static',
+                          template_folder='./templates')
+
+
+    @blueprint.route('/admin')
+    def admin():
+        return render_template('admin.html')
+
+    app.register_blueprint(blueprint)
