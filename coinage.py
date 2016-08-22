8fe93578073606c5e7ce934652f441e68dcc54e5
Casagnap <mc185104@ncr.com>
2016-08-22 14:05:01 -0400

    commit changes to set entry to coinage.py

---
+++ coinage.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,abort
+from jinja2 import TemplateNotFound
+
+app = Flask(__name__)
+from contributions.views import contributions_blueprint
+
+@app.route("/")
+def home():
+    try:
+        return render_template("home.html")
+    except TemplateNotFound:
+        abort(404)
+
+#register our blueprints
+app.register_blueprint(contributions_blueprint)
+
+if __name__ == "__main__":
+    app.run()
