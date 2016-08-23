98a36d8323e2ef170aba68e7a820f2a95ecc30b4
Casagnap <mc185104@ncr.com>
2016-08-22 22:03:38 -0400

    commit config.py

--- coinage.py
+++ coinage.py
@@ -1,9 +1,16 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 from home.views import home_blueprint
 from contributions.views import contributions_blueprint
