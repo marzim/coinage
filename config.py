76c75d4a58549067e81cec77044d596e2258bd56
Casagnap <mc185104@ncr.com>
2016-08-23 15:01:41 -0400

    commit changes

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ __author__ = 'Marvin'
 import os
 # default configuration
 
-class BaseConfig(object):
-    DEBUG = False
-    SECRET_KEY = os.environ['MYSECRETKEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-class DevelopmentConfig(BaseConfig):
-    DEBUG = True
-
-class ProductionConfig(BaseConfig):
-    DEBUG = False
+DEBUG = False
+SECRET_KEY = os.environ['MYSECRETKEY']
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# class BaseConfig(object):
+#     DEBUG = False
+#     SECRET_KEY = os.environ['MYSECRETKEY']
+#     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#
+# class DevelopmentConfig(BaseConfig):
+#     DEBUG = True
+#
+# class ProductionConfig(BaseConfig):
+#     DEBUG = False
