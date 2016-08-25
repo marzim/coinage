865e303e2a228911dbe4774b3cf29177c0b65bbf
Casagnap <mc185104@ncr.com>
2016-08-25 10:36:35 -0400

    commit changes

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = os.environ['MYSECRETKEY']
+    SECRET_KEY = 'p\xfa\xa6\x0fM\x7f\xb4o\xae\xb1\x1dv\x82\xacky\xd6\xe4\x93\x0b\x00\x86\xc6&'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
