3ce272e06685b3f5ef4146721881a31055b6cd1a
Casagnap <mc185104@ncr.com>
2016-08-26 08:48:22 -0400

    commit changes in index.html

--- coinage.py
+++ coinage.py
@@ -16,11 +16,12 @@ db = SQLAlchemy(app)
 from home.views import home_blueprint
 from contributions.views import contributions_blueprint
 from summary.views import summary_blueprint
-
+from loans.views import loans_blueprint
 #register our blueprints
 app.register_blueprint(contributions_blueprint)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(summary_blueprint)
+app.register_blueprint(loans_blueprint)
 
 from users.models import User
 
