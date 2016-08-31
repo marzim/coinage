56e548056633f3c076a6f5a6e8e56d45599ae5d3
Casagnap <mc185104@ncr.com>
2016-08-31 10:01:46 -0400

    commit changes

--- coinage.py
+++ coinage.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
