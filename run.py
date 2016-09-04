427ae11015825a8263852a2ba2cf60cde7e6423d
marzim <marvin.casagnap@gmail.com>
2016-09-04 08:38:28 -0700

    commit changes for user model

--- run.py
+++ run.py
@@ -3,8 +3,5 @@ from users.models import User
 
 if __name__ == '__main__':
     db.create_all()
-    admin = User('matt','matt@ncr.com','test')
-    db.session.add(admin)
-    db.session.commit()
     app.run(debug=True)
 
