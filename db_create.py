88bd5920d8a080b0c746fe71cc92c2c6fe2537ca
marzim <marvin.casagnap@gmail.com>
2016-09-04 19:18:44 -0700

    commit changes for db_create.py

--- db_create.py
+++ db_create.py
@@ -1,7 +1,8 @@
 from coinage import db
-
+from users.models import User
 # create the database and the db table
 db.create_all()
-
+#insert
+db.session.add(User("marzim","marzim@gmail.com", "mar_180IOP{"))
 # commit the changes
 db.session.commit()
