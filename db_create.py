34eb139f28cf2545e02e2db1bbffda3f6de8b443
marzim <marvin.casagnap@gmail.com>
2016-09-07 23:49:52 -0700

    commit changes

--- db_create.py
+++ db_create.py
@@ -1,6 +1,6 @@
-import os
+#import os
 
-os.environ['DATABASE_URL'] = "mysql+mysqldb://coinage:beer_180JKL:@coinage.mysql.pythonanywhere-services.com/coinage$savings"
+#os.environ['DATABASE_URL'] = "mysql+mysqldb://coinage:beer_180JKL:@coinage.mysql.pythonanywhere-services.com/coinage$savings"
 
 from coinage import db
 from users.models import User
@@ -8,6 +8,6 @@ from users.models import User
 # create the database and the db table
 db.create_all()
 #insert
-db.session.add(User("marzim","marzim@gmail.com", "mar_180IOP{"))
+#db.session.add(User("marzim","marzim@gmail.com", "mar_180IOP{"))
 # commit the changes
 db.session.commit()
--- run.py
+++ run.py
@@ -2,6 +2,5 @@ from coinage import app, db
 from users.models import User
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
 
