455515ebf6c6ad504a891ff75d0f93cf7095bd04
marzim <marvin.casagnap@gmail.com>
2016-09-12 12:17:20 -0700

    commit changes for create tables

--- db_create.py
+++ db_create.py
@@ -1,9 +1,9 @@
-#import os
+import os
 
-#os.environ['DATABASE_URL'] = "mysql+mysqldb://coinage:beer_180JKL:@coinage.mysql.pythonanywhere-services.com/coinage$savings"
+os.environ['DATABASE_URL'] = "mysql+mysqldb://coinage:beer_180JKL:@coinage.mysql.pythonanywhere-services.com/coinage$savings"
 
 from coinage import db
-from users.models import User
+#from users.models import User
 
 # create the database and the db table
 db.create_all()
