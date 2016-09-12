22420470b88c01a3a060574beb176560997cab5c
marzim <marvin.casagnap@gmail.com>
2016-09-12 12:19:08 -0700

    commit changes for create tables

--- db_create.py
+++ db_create.py
@@ -3,11 +3,11 @@ import os
 os.environ['DATABASE_URL'] = "mysql+mysqldb://coinage:beer_180JKL:@coinage.mysql.pythonanywhere-services.com/coinage$savings"
 
 from coinage import db
-#from users.models import User
+from users.models import User
 
 # create the database and the db table
 db.create_all()
 #insert
-#db.session.add(User("marzim","marzim@gmail.com", "mar_180IOP{"))
+db.session.add(User("admin","marzim@gmail.com", "password"))
 # commit the changes
 db.session.commit()
