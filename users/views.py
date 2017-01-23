d0484de8a63268f4faa02db36c530c73b5354d60
marzim <marvin.casagnap@gmail.com>
2017-01-23 21:14:03 +0800

    commit changes in showing users list and some icons for edit and delete

--- users/views.py
+++ users/views.py
@@ -1,27 +1,48 @@
 from flask import Blueprint, render_template, abort, flash, request, url_for, redirect
 from jinja2 import TemplateNotFound
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 
 from .forms import LoginForm, RegisterForm
 
 users_blueprint = Blueprint('users', __name__, static_url_path='/users/static', static_folder='./static',
                       template_folder='./templates')
 
-@users_blueprint.route("/users")
+@users_blueprint.route("/users", methods=['GET'])   # pragma: no cover
 @login_required
 def users():
+    from models import User
     try:
-        return render_template('users.html')
+        query_users = User.query.order_by(User.name)
+        return render_template('users.html', query_users=query_users)
     except TemplateNotFound:
         abort(404)
 
-@users_blueprint.route("/users/edit")
+@users_blueprint.route("/users/edit/<id>", methods=['GET', 'POST'])   # pragma: no cover)
 @login_required
-def edituser():
-    try:
-        return render_template("edituser.html")
-    except TemplateNotFound:
-        abort(404)
+def edituser(id):
+    from coinage import db
+    from models import User
+    if not current_user.name == 'admin':
+        return redirect(url_for('users.users'))
+    user = User.query.filter_by(id=id).first()
+    form = RegisterForm()
+    form.username.data = user.name
+    form.email.data = user.email
+    error = None
+    if form.validate_on_submit():
+        if user is None:
+            error = 'User name ' + form.username.data + ' cannot be found.'
+        else:
+            db.session.commit()
+            return redirect(url_for('users.users'))
+    return render_template('register.html', form=form, error=error, instruction='Edit user', title='Edit User')
+
+@users_blueprint.route("/users/delete/<id>", methods=['POST'])   # pragma: no cover)
+@login_required
+def deleteuser(id):
+    if not current_user.name == 'admin':
+        return redirect(url_for('users.users'))
+    abort(404)
 
 @users_blueprint.route('/login', methods=['GET', 'POST'])   # pragma: no cover
 def login():
@@ -74,4 +95,4 @@ def register():
             return redirect(url_for('home.home'))
         else:
             error = 'User name ' + form.username.data + ' is already taken.'
-    return render_template('register.html', form=form, error=error)
+    return render_template('register.html', form=form, error=error, instruction='Please register', title='Register')
