0135f94d2c6e117de654776047f7879fd7ea7b5b
marzim <marvin.casagnap@gmail.com>
2017-02-03 21:47:44 +0800

    commit changes on edit and delete users

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'p\xfa\xa6\x0fM\x7f\xb4o\xae\xb1\x1dv\x82\xacky\xd6\xe4\x93\x0b\x00\x86\xc6&'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
