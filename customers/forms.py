ab08cfbfa6d2df89527924b86b8d080f94c45d63
marzim <marvin.casagnap@gmail.com>
2017-02-10 20:31:57 +0800

    commit changes in customers CRUD

--- customers/forms.py
+++ customers/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, IntegerField
-from wtforms.validators import Length, Email, InputRequired
+from wtforms.validators import Length, Email, InputRequired, DataRequired
 
 class AddForm(Form):
     first_name = StringField(
@@ -29,23 +29,23 @@ class AddForm(Form):
 class EditForm(Form):
     first_name = StringField(
         'firstname',
-        validators=[InputRequired(), Length(min=3, max=25)]
+        validators=[DataRequired(), Length(min=3, max=25)]
     )
     last_name = StringField(
         'lastname',
-        validators=[InputRequired(), Length(min=3, max=25)]
+        validators=[DataRequired(), Length(min=3, max=25)]
     )
     number_shares = IntegerField('numbershares',
                                  validators=[InputRequired()])
     email = StringField(
         'email',
-        validators=[InputRequired(), Email(message=None), Length(min=6, max=40)]
+        validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]
     )
     address = StringField(
         'address',
-        validators=[InputRequired(), Length(min=3, max=25)]
+        validators=[DataRequired(), Length(min=3, max=25)]
     )
     mobile_phone = StringField(
         'mobilephone',
-        validators=[InputRequired(), Length(min=3, max=25)]
+        validators=[DataRequired(), Length(min=3, max=25)]
     )
